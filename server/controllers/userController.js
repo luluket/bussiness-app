ae6a44e396ab602b0a5932bb92461ce7f1de0585
Luka <luka.luketin@holo-one.com>
2021-07-01 19:53:00 +0200

    User Registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -23,6 +23,37 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  //get data from body
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
@@ -41,4 +72,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
