dec9863e6d398fa5323d7dec819882e6b23a688f
Luka <luka.luketin@holo-one.com>
2021-07-27 01:44:12 +0200

    Admin User List, Admin Screen Access Security, Admin User Delete

--- client/src/store.js
+++ client/src/store.js
@@ -10,6 +10,8 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
+  userDeleteReducer,
 } from "./reducers/userReducers";
 
 import {
@@ -27,6 +29,8 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
