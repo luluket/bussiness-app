8f3462af3c3570bdb47347567af62b83e65f8031
Luka <luka.luketin@holo-one.com>
2021-07-29 13:24:04 +0200

    Product Image Upload

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import products from "./data/products.js";
 import connectDB from "./config/db.js";
@@ -8,6 +9,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -21,13 +23,17 @@ app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.use("/api/products", productRoutes); // anything that goes to that routes calles that module
+app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
-); // use this route upon paying
+);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 
