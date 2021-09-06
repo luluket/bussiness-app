4daba96dd2c3aaeeac35194be5eb7ac840cb6f62
Luka <luka.luketin@holo-one.com>
2021-09-06 14:04:20 +0200

    trying to fix production mode

--- server/server.js
+++ server/server.js
@@ -63,7 +63,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "/client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
